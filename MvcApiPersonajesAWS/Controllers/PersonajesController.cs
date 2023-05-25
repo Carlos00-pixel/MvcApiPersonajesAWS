9a91dd45638122943679932a36eaa5b6bd62c5a6
Carlos00-pixel <58859997+Carlos00-pixel@users.noreply.github.com>
2023-05-25 14:27:48 +0200

    Commit

--- MvcApiPersonajesAWS/Controllers/PersonajesController.cs
+++ MvcApiPersonajesAWS/Controllers/PersonajesController.cs
@@ -1,9 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MvcApiPersonajesAWS.Services;
 
 namespace MvcApiPersonajesAWS.Controllers
 {
     public class PersonajesController : Controller
     {
+        private ServicePersonajes service;
+
+        public PersonajesController(ServicePersonajes service)
+        {
+            this.service = service;
+        }
+
+        public IActionResult Cliente()
+        {
+            return View();
+        }
+
         public IActionResult Personajes()
         {
             return View();
