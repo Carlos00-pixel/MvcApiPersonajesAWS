e908a3cc8ed0d5fa1356c7481294ef744338b2c7
Carlos00-pixel <58859997+Carlos00-pixel@users.noreply.github.com>
2023-05-26 12:42:04 +0200

    Commit SSL

--- MvcApiPersonajesAWS/Services/ServicePersonajes.cs
+++ MvcApiPersonajesAWS/Services/ServicePersonajes.cs
@@ -35,21 +35,30 @@ namespace MvcApiPersonajesAWS.Services
 
         private async Task<T> CallApiAsync<T>(string request)
         {
-            using (HttpClient client = new HttpClient())
+            using (HttpClientHandler handler = new HttpClientHandler())
             {
-                client.BaseAddress = new Uri(this.UrlApi);
-                client.DefaultRequestHeaders.Clear();
-                client.DefaultRequestHeaders.Accept.Add(this.Header);
-                HttpResponseMessage response =
-                    await client.GetAsync(request);
-                if (response.IsSuccessStatusCode)
+                handler.ServerCertificateCustomValidationCallback =
+                (message, cert, chain, sslPolicyErrors) =>
                 {
-                    T data = await response.Content.ReadAsAsync<T>();
-                    return data;
-                }
-                else
+                    return true;
+                };
+
+                using (HttpClient client = new HttpClient())
                 {
-                    return default(T);
+                    client.BaseAddress = new Uri(this.UrlApi);
+                    client.DefaultRequestHeaders.Clear();
+                    client.DefaultRequestHeaders.Accept.Add(this.Header);
+                    HttpResponseMessage response =
+                        await client.GetAsync(request);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        T data = await response.Content.ReadAsAsync<T>();
+                        return data;
+                    }
+                    else
+                    {
+                        return default(T);
+                    }
                 }
             }
         }
